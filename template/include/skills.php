a9d16dced724f8ff2e35f1cd82948f1a653b9e61
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2022-08-03 03:09:51 +0200

    Ajout des barre de skill en html et des animation ainsi que des variable de couleurs en css

--- template/include/skills.php
+++ template/include/skills.php
@@ -1,15 +1,53 @@
 <div id="section_two">
   <div id="left_skill">
       <div id="all_my_skills_left">
+
         <div class="progress">
-          <div class="progress-value"></div>
+          <label for="prgr_html">HTML/CSS/Bootstrap</label>
+          <div id="prgr_html" class="progress-value"></div>
         </div>
+
+        <!-- <label for="prgr_bootstrap">Bootstrap</label> -->
+        <!-- <div class="progress">
+          <div id="prgr_bootstrap" class="progress-value"></div>
+        </div> -->
+
         <div class="progress">
-          <div class="progress-value"></div>
+          <label for="prgr_js">JavaScript</label>
+          <div id="prgr_js" class="progress-value"></div>
         </div>
+
         <div class="progress">
-          <div class="progress-value"></div>
+          <label for="prgr_java">Java/SpringBoot</label>
+          <div id="prgr_java" class="progress-value"></div>
         </div>
+
+        <!-- <label for="prgr_js">JavaScript</label> -->
+        <div class="progress">
+          <label for="prgr_api">API REST/ SOAP</label>
+          <div id="prgr_api" class="progress-value"></div>
+        </div>
+
+        <div class="progress">
+          <label for="prgr_docker">Docker</label>
+          <div id="prgr_docker" class="progress-value"></div>
+        </div>
+
+        <div class="progress">
+          <label for="prgr_js">Thymleaf</label>
+          <div id="prgr_thmlf" class="progress-value"></div>
+        </div>
+
+        <div class="progress">
+          <label for="prgr_sql">MySql/PHP/SQL Server</label>
+          <div id="prgr_sql" class="progress-value"></div>
+        </div>
+
+        <div class="progress">
+          <label for="prgr_srv">Apache TomCat/WampServer</label>
+          <div id="prgr_srv" class="progress-value"></div>
+        </div>
+
         <!-- <div class="ldBar label-center" data-preset="bubble" data-value="75"></div> -->
       </div>
   </div>
