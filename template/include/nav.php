24c705c5b491c8a5128d5d27c7e2ae177f12985f
xxStevensxx <29457062+xxStevensxx@users.noreply.github.com>
2022-08-08 01:44:45 +0200

    ajout des imgs des projets generiques, modification du css lié à la page, modification de l'arguments au clique dans la barre de nav

--- template/include/nav.php
+++ template/include/nav.php
@@ -4,7 +4,7 @@
     <li id="a_propos"><a href="#one" onclick="move(section_one,[triangle_bot_left_shape,right_desc_about_me])">A propos de moi</a></li>
     <li id="skill"><a href="#two" onclick="move(section_two,[all_my_skills_left,triangle_top_right_shape])">skills</a></li>
     <li id="experiences"><a href="#three" onclick="move(section_three,[center])">experiences</a></li>
-    <li id="projet"><a href="#four" onclick="move(section_four,[caroussel])">projets</a></li>
+    <li id="projet"><a href="#four" onclick="move(section_four,[content_four])">projets</a></li>
     <li id="bonus"><a href="#five">bonus</a></li>
   </ul>
 </nav>
